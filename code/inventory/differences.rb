0f77cfb8364df4098e3371535ca2f856a2c7184d
Cyril Ledru <patate@bibi-nux-desktop.(none)>
2010-06-27 23:24:16 +0200

    Inventory file names as arguments

--- code/inventory/differences.rb
+++ code/inventory/differences.rb
@@ -1,5 +1,5 @@
-old_inventory = File.open('old-inventory.txt').readlines
-new_inventory = File.open('new-inventory.txt').readlines
+old_inventory = File.open(ARGV[0]).readlines
+new_inventory = File.open(ARGV[1]).readlines
 
 puts "The following files have been added:"
 puts new_inventory - old_inventory
