ffe1d128f9d3dc0695fe99443fa5fb095b05edba
Cyril Ledru <cyril.ledru@gmail.com>
2010-06-27 23:45:21 +0200

    Fixed Windows case bug
    By loading lines as lower case string

--- code/inventory/differences.rb
+++ code/inventory/differences.rb
@@ -2,8 +2,13 @@ unless ARGV.length == 2
 	puts "Usage: differences.rb old-inventory new-inventory"
 	exit
 end
-old_inventory = File.open(ARGV[0]).readlines
-new_inventory = File.open(ARGV[1]).readlines
+
+old_inventory = File.open(ARGV[0]).collect do |line|
+	line.downcase
+end
+new_inventory = File.open(ARGV[1]).collect do |line|
+	line.downcase
+end
 
 puts "The following files have been added:"
 puts new_inventory - old_inventory
