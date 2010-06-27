e8d1fac5607648118d5d1feeedd562e4e123f279
Cyril Ledru <cyril.ledru@gmail.com>
2010-06-27 23:50:45 +0200

    Removed duplication
    Extracted a method for loading lower case inventory

--- code/inventory/differences.rb
+++ code/inventory/differences.rb
@@ -1,14 +1,16 @@
+def inventory_from(filename)
+	File.open(filename).collect do |line|
+		line.downcase
+	end
+end
+
 unless ARGV.length == 2
 	puts "Usage: differences.rb old-inventory new-inventory"
 	exit
 end
 
-old_inventory = File.open(ARGV[0]).collect do |line|
-	line.downcase
-end
-new_inventory = File.open(ARGV[1]).collect do |line|
-	line.downcase
-end
+old_inventory = inventory_from(ARGV[0])
+new_inventory = inventory_from(ARGV[1])
 
 puts "The following files have been added:"
 puts new_inventory - old_inventory
