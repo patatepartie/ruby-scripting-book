a0be070c76186a8c31cb734a7df4b51ee41b48e4
Cyril Ledru <cledru@keynectis.net>
2010-12-09 13:51:01 +0100

    Make script loadable in irb
    Use the $0 == __FILE__ syntaxe.
    Encapsulate usage and script body into methods.
    Fix indentation error in output.

--- code/inventory/differences.rb
+++ code/inventory/differences.rb
@@ -1,6 +1,8 @@
-unless ARGV.length == 2
-  puts "Usage: differences.rb old-inventory new-inventory"
-  exit
+def check_usage
+  unless ARGV.length == 2
+    puts "Usage: differences.rb old-inventory new-inventory"
+    exit
+  end
 end
 
 def boring?(line)
@@ -17,26 +19,33 @@ def inventory_from(filename)
   end
 end
 
-old_inventory = inventory_from(ARGV[0])
-new_inventory = inventory_from(ARGV[1])
+def compare_inventory_files(old_file, new_file)
+  old_inventory = inventory_from(old_file)
+  new_inventory = inventory_from(new_file)
 
-added_files_count = (new_inventory - old_inventory).length
-puts "The following "
-puts added_files_count
-puts " files have been added:"
+  added_files_count = (new_inventory - old_inventory).length
+  puts "The following "
+  puts added_files_count
+  puts "file(s) have been added:"
 
-puts new_inventory - old_inventory
+  puts new_inventory - old_inventory
 
-puts ""
-removed_files_count = (old_inventory - new_inventory).length
-puts "The following "
-puts removed_files_count
-puts " files have been deleted:"
-puts old_inventory - new_inventory
+  puts ""
+  removed_files_count = (old_inventory - new_inventory).length
+  puts "The following "
+  puts removed_files_count
+  puts "file(s) have been deleted:"
+  puts old_inventory - new_inventory
 
-puts ""
-unchanged_files_count = (old_inventory - (old_inventory - new_inventory)).length
-puts "The following "
-puts unchanged_files_count
-puts " files have not changed:"
-puts old_inventory - (old_inventory - new_inventory)
+  puts ""
+  unchanged_files_count = (old_inventory - (old_inventory - new_inventory)).length
+  puts "The following "
+  puts unchanged_files_count
+  puts "file(s) have not changed:"
+  puts old_inventory - (old_inventory - new_inventory)
+end
+
+if $0 == __FILE__
+  check_usage
+  compare_inventory_files(ARGV[0], ARGV[1])
+end
