f352a5729c7454fa3491ad9215ff3c85aa7bacc7
Cyril Ledru <cledru@keynectis.net>
2010-12-09 12:19:30 +0100

    Usage message

--- code/inventory/differences.rb
+++ code/inventory/differences.rb
@@ -1,3 +1,8 @@
+unless ARGV.length == 2
+  puts "Usage: differences.rb old-inventory new-inventory"
+  exit
+end
+
 old_inventory = File.open(ARGV[0]).readlines
 new_inventory = File.open(ARGV[1]).readlines
 
