11a4debaccdad1e50871f93c3b26234d579c48e6
Cyril Ledru <cledru@keynectis.net>
2010-12-09 12:47:27 +0100

    Does not compare files in temp or recycler

--- code/inventory/differences.rb
+++ code/inventory/differences.rb
@@ -3,9 +3,17 @@ unless ARGV.length == 2
   exit
 end
 
+def boring?(line)
+  line.split('/').include?('temp') or line.split('/').include?('recycler')
+end
+
 def inventory_from(filename)
-  File.open(filename).collect do |line|
+  inventory = File.open(filename)
+  downcased = inventory.collect do |line|
     line.downcase
+  end
+  downcased.reject do |line|
+    boring?(line)
   end
 end
 
