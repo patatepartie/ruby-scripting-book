dcee6cb0a02589d256b0587218c71b4f6b84e161
Cyril <cyril.ledru@gmail.com>
2010-12-22 22:42:13 +0100

    Use snapshot version as base

--- code/churn/churn.rb
+++ code/churn/churn.rb
@@ -3,76 +3,74 @@
 # We make no guarantees that this code is fit for any purpose. 
 # Visit http://www.pragmaticprogrammer.com/titles/bmsft for more book information.
 #---
+
+class SubversionRepository
+  def date(a_time)
+    a_time.strftime("%Y-%m-%d")
+  end
+
+  def change_count_for(name, start_date)
+    extract_change_count_from(log(name, start_date))
+  end
+
+  def extract_change_count_from(log_text)
+    lines = log_text.split("\n")
+    dashed_lines = lines.find_all do | line |
+      line.include?('--------') 
+    end
+    dashed_lines.length - 1     
+  end
+
+  def log(subsystem, start_date)
+    timespan = "--revision 'HEAD:{#{start_date}}'"
+    root = "svn://rubyforge.org//var/svn/churn-demo"
+    
+    `svn log #{timespan} #{root}/#{subsystem}`
+  end
+end
+
 def month_before(a_time)
   a_time - 28 * 24 * 60 * 60
 end
 
-def svn_date(a_time)
-  a_time.strftime("%Y-%m-%d")
-end
-
-def header(svn_start_date, svn_end_date)
-  "Changes between #{svn_start_date} and #{svn_end_date}:"
+def header(a_date)
+  "Changes since #{a_date}:"
 end
 
 def subsystem_line(subsystem_name, change_count)
   asterisks = asterisks_for(change_count)
-  changes = changes_for(change_count)
-  "#{subsystem_name.ljust(14)} #{changes.ljust(14)} #{asterisks}"
-end
-
-def changes_for(change_count)
-  if change_count == 0
-    "-"
-  else
-    "(#{change_count} changes)"
-  end
+  "#{subsystem_name.rjust(14)} #{asterisks} (#{change_count})"
 end
 
 def asterisks_for(an_integer)
-  if an_integer == 0
-    '-'
-  elsif an_integer <= 2
-    '*'
-  else
-    '*' * (an_integer / 5.0).round
-  end
-end
-
-def change_count_for(name, start_date)
-  extract_change_count_from(svn_log(name, start_date))
+  '*' * (an_integer / 5.0).round
 end
 
 
-def extract_change_count_from(log_text)
-  lines = log_text.split("\n")
-  dashed_lines = lines.find_all do | line |
-    line.include?('--------') 
+def order_by_descending_change_count(lines)
+  lines.sort do | one, another |
+    one_count = churn_line_to_int(one)
+    another_count = churn_line_to_int(another)
+    - (one_count <=> another_count)
   end
-  count = dashed_lines.length - 1
-  if count < 0
-    message = "Subversion returned no log. Is the network down?"
-    raise Exception.new(message)
-  end
-  count
 end
 
-def svn_log(subsystem, start_date)
-  timespan = "--revision 'HEAD:{#{start_date}}'"
-  root = "svn://rubyforge.org//var/svn/churn-demo"
-    
-  `svn log #{timespan} #{root}/#{subsystem}`
+def churn_line_to_int(line)
+  /\((\d+)\)/.match(line)[1].to_i
 end
 
 
 if $0 == __FILE__
   subsystem_names = ['audit', 'fulfillment', 'persistence',
                      'ui', 'util', 'inventory']
-  start_date = svn_date(month_before(Time.now))
-  end_date = svn(Time.now)
+  repository = SubversionRepository.new #(1)
+  start_date = repository.date(month_before(Time.now)) #(2)
 
-  puts header(start_date, end_date)
-  subsystem_names.each do | name |
-    puts subsystem_line(name, change_count_for(name, start_date)) 
+  puts header(start_date)
+  lines = subsystem_names.collect do | name |
+    subsystem_line(name,
+                   repository.change_count_for(name, start_date)) #(3)
   end
+  puts order_by_descending_change_count(lines)
 end
+
